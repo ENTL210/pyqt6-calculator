3a8c47b53d1f796d8e604c22f5f084ef1809ab18
ENTL210 <edwardlamhitech@gmail.com>
2024-03-18 15:38:06 -0700

    Working on the UI

--- pyqt6_calculator/app.py
+++ pyqt6_calculator/app.py
@@ -3,10 +3,11 @@ from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
-    QVBoxLayout,
-    QHBoxLayout,
+    QGridLayout,
     QWidget,
-    QLabel
+    QDoubleSpinBox,
+    QLabel,
+    
 )
 
 class MainWindow(QMainWindow):
@@ -14,25 +15,39 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Mortgage Calculator")
-        self.setMinimumSize(QSize(450,500 ))
+        self.setMinimumSize(QSize(600,200 ))
 
-        main_layout = QVBoxLayout()
+        main_layout = QGridLayout()
 
         main = QWidget()
         main.setLayout(main_layout)
 
         self.setCentralWidget(main)
-
+        
+        # Header...
         header = QLabel("Mortgage Calculator")
         header.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         header.setFont(QFont("Helvetica", 25))
-        main_layout.addWidget(header)
-
-        calculator_container = QWidget()
-        calculator_layout = QHBoxLayout()
-        calculator_container.setLayout(calculator_layout)
-
-        main_layout.addWidget(calculator_container)
+        main_layout.addWidget(header, 0, 0, 1, 5)
+        
+        loan_amount_label = QLabel("Loan Amount")
+        loan_amount_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        loan_amount_label.setFont(QFont("Helvetica", 18))
+        main_layout.addWidget(loan_amount_label, 1, 0)
+        
+        loan_amount_doubleSpinBox = QDoubleSpinBox()
+        loan_amount_doubleSpinBox.setMinimum(0.0)
+        loan_amount_doubleSpinBox.setMaximum(float("inf"))
+        loan_amount_doubleSpinBox.setPrefix("$ ")
+        loan_amount_doubleSpinBox.setSingleStep(1000.0)
+        loan_amount_doubleSpinBox.setMaximumSize(200, 50)
+        loan_amount_doubleSpinBox.setFont(QFont("Helvetica", 15))
+        
+        main_layout.addWidget(loan_amount_doubleSpinBox, 2, 0, 1, 2)
+        
+        
+        
+        
 
 
 app = QApplication([])
