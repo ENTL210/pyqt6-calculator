9bef449f641837b4362b405344d699bbfad6b3c0
ENTL210 <edwardlamhitech@gmail.com>
2024-03-22 23:04:22 -0700

    Working on the UI

--- pyqt6_calculator/app.py
+++ pyqt6_calculator/app.py
@@ -8,6 +8,7 @@ from PyQt6.QtWidgets import (
     QGridLayout,
     QWidget,
     QGroupBox,
+    QSpinBox,
     QDoubleSpinBox,
     QLabel,
     
@@ -18,45 +19,67 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Mortgage Calculator")
-        self.setMinimumSize(QSize(600,200 ))
 
-        main_layout = QGridLayout()
-
-        main = QWidget()
-        main.setLayout(main_layout)
-
-        self.setCentralWidget(main)
+        self.UiComponents()
         
+    def UiComponents(self):
+        self.main_layout = QGridLayout()
+        self.main_layout.setSpacing(40)
+
+        self.main = QWidget()
+        self.main.setLayout(self.main_layout)
+
+        self.setCentralWidget(self.main)
         # Header...
-        header = QLabel("Mortgage Calculator")
-        header.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        header.setFont(QFont("Helvetica", 25))
-        main_layout.addWidget(header, 0, 0, 1, 5)
+        self.header = QLabel("Mortgage Calculator")
+        self.header.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.header.setFont(QFont("Helvetica", 25))
+        self.main_layout.addWidget(self.header, 0, 0, 1, 5)
         
-        loan_amount_groupBox = QGroupBox("Loan Amount")
-        loan_amount_groupBox_layout = QVBoxLayout()
-        loan_amount_groupBox.setLayout(loan_amount_groupBox_layout)
+        self.loan_amount_groupBox = QGroupBox("Loan Amount")
+        self.loan_amount_groupBox_layout = QVBoxLayout()
+        self.loan_amount_groupBox.setLayout(self.loan_amount_groupBox_layout)
      
-        loan_amount_doubleSpinBox = QDoubleSpinBox()
-        loan_amount_doubleSpinBox.setMinimum(0.0)
-        loan_amount_doubleSpinBox.setMaximum(float("inf"))
-        loan_amount_doubleSpinBox.setPrefix("$ ")
-        loan_amount_doubleSpinBox.setSingleStep(1000.0)
-        loan_amount_doubleSpinBox.setMaximumSize(200, 50)
+        self.loan_amount_doubleSpinBox = QDoubleSpinBox()
+        self.loan_amount_doubleSpinBox.setMinimum(0.0)
+        self.loan_amount_doubleSpinBox.setMaximum(float("inf"))
+        self.loan_amount_doubleSpinBox.setPrefix("$ ")
+        self.loan_amount_doubleSpinBox.setFont(QFont("Helvetica", 15))
+        self.loan_amount_doubleSpinBox.setSingleStep(1000.0)
+        self.loan_amount_doubleSpinBox.setMinimumSize(200, 50)
+        self.loan_amount_doubleSpinBox.valueChanged.connect(self.set_down_payment_max)
         
-        loan_amount_groupBox_layout.addWidget(loan_amount_doubleSpinBox)
-        main_layout.addWidget(loan_amount_groupBox, 1, 0, 1, 2)
+        self.loan_amount_groupBox_layout.addWidget(self.loan_amount_doubleSpinBox)
+        self.main_layout.addWidget(self.loan_amount_groupBox, 1, 0, 1, 2)
         
-        down_amount_groupBox = QGroupBox("Down Amount")
-        down_amount_groupBox_layout = QHBoxLayout()
-        down_amount_groupBox.setLayout(down_amount_groupBox_layout)
+        self.down_amount_groupBox = QGroupBox("Down Amount")
+        self.down_amount_groupBox_layout = QHBoxLayout()
+        self.down_amount_groupBox.setLayout(self.down_amount_groupBox_layout)
         
-        down_amount_doubleSpinBox = QDoubleSpinBox()
-        down_amount_doubleSpinBox.setMinimum(0,0)
-        down_amount_doubleSpinBox.setMaximum()
+        self.down_amount_doubleSpinBox = QDoubleSpinBox()
+        self.down_amount_doubleSpinBox.setMinimum(0.0)
+        self.down_amount_doubleSpinBox.setMaximum(self.loan_amount_doubleSpinBox.value())
+        self.down_amount_doubleSpinBox.setPrefix("$ ")
+        self.down_amount_doubleSpinBox.setFont(QFont("Helvetica", 15))
+        self.down_amount_doubleSpinBox.setSingleStep(1000.0)
+        self.down_amount_doubleSpinBox.setMinimumSize(200, 50)
         
+        self.down_percent_spinBox = QSpinBox()
+        self.down_percent_spinBox.setMinimum(0)
+        self.down_percent_spinBox.setMaximum(100)
+        self.down_percent_spinBox.setSuffix(" %")
+        self.down_percent_spinBox.setFont(QFont("Helvetica", 15))
+        self.down_percent_spinBox.setSingleStep(5)
+        self.down_percent_spinBox.setMinimumSize(100,50)
         
+        self.down_amount_groupBox_layout.addWidget(self.down_amount_doubleSpinBox)
+        self.down_amount_groupBox_layout.addWidget(self.down_percent_spinBox)
+        self.main_layout.addWidget(self.down_amount_groupBox, 2, 0, 1, 2)
         
+    def set_down_payment_max(self):
+        value = self.loan_amount_doubleSpinBox.value()
+        self.down_amount_doubleSpinBox.setMaximum(value)
+        self.down_amount_doubleSpinBox.setValue(value * 0.05)
         
 
 
