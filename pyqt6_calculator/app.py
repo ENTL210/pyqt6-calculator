ece0e91f168157b93daffd84b0a84c54e09fc0d6
ENTL210 <edwardlamhitech@gmail.com>
2024-05-31 09:36:01 -0700

    Adding comments to the code

--- pyqt6_calculator/app.py
+++ pyqt6_calculator/app.py
@@ -29,10 +29,15 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Mortgage Calculator")
 
+        # Call the main UI...
         self.UiComponents()
         
     def UiComponents(self):
+        """
+        A function that render out the UI of the app
+        """
 
+        # Main container...
         self.main = QWidget()
         self.main_layout = QGridLayout()
         self.main_layout.setSpacing(40)
@@ -40,6 +45,7 @@ class MainWindow(QMainWindow):
         self.main.setLayout(self.main_layout)
         self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        # Set the container as the main widget of the window...
         self.setCentralWidget(self.main)
         
         # Header...
@@ -48,65 +54,95 @@ class MainWindow(QMainWindow):
         self.header.setFont(QFont("Helvetica", 15))
         self.main_layout.addWidget(self.header, 0, 0, 1, 5)
         
+        # Loan Amount Entrybox Container...
         self.loan_amount_groupBox = QGroupBox("Loan Amount")
         self.loan_amount_groupBox_layout = QVBoxLayout()
         self.loan_amount_groupBox.setLayout(self.loan_amount_groupBox_layout)
-     
+
+        # Loan Amount Entrybox...
         self.loan_amount_doubleSpinBox = QDoubleSpinBox()
+        
+        # Set the parameters of the entrybox...
         self.loan_amount_doubleSpinBox.setMinimum(0.0)
         self.loan_amount_doubleSpinBox.setMaximum(float("inf"))
         self.loan_amount_doubleSpinBox.setPrefix("$ ")
         self.loan_amount_doubleSpinBox.setFont(QFont("Helvetica", 12))
         self.loan_amount_doubleSpinBox.setSingleStep(1000.0)
         self.loan_amount_doubleSpinBox.setMinimumSize(200, 50)
+        
+        # When the loan amount entrybox's value is changing...
         self.loan_amount_doubleSpinBox.valueChanged.connect(self.update_down_payment_value)
         
+        # Add the loan entrybox to the layout...
         self.loan_amount_groupBox_layout.addWidget(self.loan_amount_doubleSpinBox)
         self.main_layout.addWidget(self.loan_amount_groupBox, 1, 0, 1, 2)
         
+        # Create down amount entrybox container...
         self.down_amount_groupBox = QGroupBox("Down Amount")
         self.down_amount_groupBox_layout = QHBoxLayout()
         self.down_amount_groupBox.setLayout(self.down_amount_groupBox_layout)
         
+        # Create down amount entrybox...
         self.down_amount_doubleSpinBox = QDoubleSpinBox()
+        
+        # Set the parameters of the entrybox...
         self.down_amount_doubleSpinBox.setMinimum(0.0)
         self.down_amount_doubleSpinBox.setMaximum(self.loan_amount_doubleSpinBox.value())
         self.down_amount_doubleSpinBox.setPrefix("$ ")
         self.down_amount_doubleSpinBox.setFont(QFont("Helvetica", 12))
         self.down_amount_doubleSpinBox.setSingleStep(1000.0)
         self.down_amount_doubleSpinBox.setMinimumSize(200, 50)
+        
+        # When the down amount is changing...
         self.down_amount_doubleSpinBox.valueChanged.connect(self.update_down_percent_value)
         
+        # Create down percent entrybox...
         self.down_percent_doubleSpinBox = QDoubleSpinBox()
+        
+        # Set the parameters of the entrybox...
         self.down_percent_doubleSpinBox.setMinimum(5)
         self.down_percent_doubleSpinBox.setMaximum(100)
         self.down_percent_doubleSpinBox.setSuffix(" %")
         self.down_percent_doubleSpinBox.setFont(QFont("Helvetica", 12))
         self.down_percent_doubleSpinBox.setSingleStep(5)
         self.down_percent_doubleSpinBox.setMinimumSize(100,50)
+        
+        # When the value of down percent is changing.
         self.down_percent_doubleSpinBox.valueChanged.connect(self.update_down_payment_value)
         
+        # Add the down amount and down percent entrybox to the layout...
         self.down_amount_groupBox_layout.addWidget(self.down_amount_doubleSpinBox)
         self.down_amount_groupBox_layout.addWidget(self.down_percent_doubleSpinBox)
         self.main_layout.addWidget(self.down_amount_groupBox, 2, 0, 1, 2)
         
+        # Create loan duration container...
         self.loan_duration_groupBox = QGroupBox("Loan Duration")
         self.loan_duration_groupBox_layout = QVBoxLayout()
         self.loan_duration_groupBox.setLayout(self.loan_duration_groupBox_layout)
         
+        # Create loan duration entrybox
         self.loan_duration_comboBox = QComboBox()
+        
+        # Initialize the options for the combobox...
         self.loan_duration_comboBox.addItems(["10 years", "15 years", "20 years", "30 years"])
+        
+        # Set the parameters for the comboxbox...
         self.loan_duration_comboBox.setMinimumSize(200, 50)
         self.loan_duration_comboBox.setFont(QFont("Helvetica", 12))
         
+        # Add the combobox to the layout...
         self.loan_duration_groupBox_layout.addWidget(self.loan_duration_comboBox)
         self.main_layout.addWidget(self.loan_duration_groupBox, 3, 0, 1, 2)
         
+        # Create interest rate entrybox container...
         self.interest_rate_groupBox = QGroupBox("Interest Rate")
         self.interest_rate_groupBox_layout = QHBoxLayout()
         self.interest_rate_groupBox.setLayout(self.interest_rate_groupBox_layout)
         
+        # Create the interest rate entrybox...
         self.interest_rate_doubleSpinBox = QDoubleSpinBox()
+        
+        # Set the parameters of the entrybox...
         self.interest_rate_doubleSpinBox.setValue(7.67)
         self.interest_rate_doubleSpinBox.setMinimum(0.0)
         self.interest_rate_doubleSpinBox.setSuffix(" %")
@@ -114,39 +150,59 @@ class MainWindow(QMainWindow):
         self.interest_rate_doubleSpinBox.setSingleStep(0.1)
         self.interest_rate_doubleSpinBox.setMinimumSize(200, 50)
         
+        # Add the entrybox to the layout...
         self.interest_rate_groupBox_layout.addWidget(self.interest_rate_doubleSpinBox)
         self.main_layout.addWidget(self.interest_rate_groupBox, 4, 0, 1, 2)
         
+        # Create the calculate button...
         self.calculate_pushBtn = QPushButton("Calculate")
+        
+        # Set the parameters of the button...
         self.calculate_pushBtn.setMinimumSize(250, 50)
+        
+        # When the button is being tapped...
         self.calculate_pushBtn.clicked.connect(self.calculate_output)
+        
+        # Add the button to the layout
         self.main_layout.addWidget(self.calculate_pushBtn, 5, 0, 1, 2)
         
+        # Create a vertical line...
         self.line = QFrame()
         self.line.setGeometry(QRect(60, 110, 751, 20))
         self.line.setFrameShape(QFrame.Shape.VLine)
         self.line.setFrameShadow(QFrame.Shadow.Sunken)
+        
+        # Add the line to the layout...
         self.main_layout.addWidget(self.line, 1, 2, 5, 1)
         
+        # Create the output container...
         self.output_container = QWidget() 
+        
+        # Set the parameters of the output container...
         self.output_container_layout = QVBoxLayout()
         self.output_container_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.output_container.setLayout(self.output_container_layout)
         self.output_container.setMinimumSize(400, 500)
+        
+        # Add the output container to the layout...
         self.main_layout.addWidget(self.output_container, 1, 3, 5, 2)
         
+        # Create the output label...
         self.monthy_payment_label = QLabel("Monthly Payment")
+        self.monthly_payment_output_label = QLabel("N/A")
+    
+        # Set the parameters of the labels...
         self.monthy_payment_label.setFont(QFont("Helvetica", 15))
         self.monthy_payment_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.output_container_layout.addWidget(self.monthy_payment_label)
-        
-        self.monthly_payment_output_label = QLabel("N/A")
         self.monthly_payment_output_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         self.monthly_payment_output_label.setFont(QFont("Helvetica", 18))
+        
+        # Add the labels to the layout...
+        self.output_container_layout.addWidget(self.monthy_payment_label)
         self.output_container_layout.addWidget(self.monthly_payment_output_label)
         
     def update_down_payment_value(self):
-        # Fetch value from the users...
+        """Update the down amount when the down % is changing..."""
         loan_amount = self.loan_amount_doubleSpinBox.value()
         down_percent = self.down_percent_doubleSpinBox.value()
 
@@ -154,9 +210,8 @@ class MainWindow(QMainWindow):
         self.down_amount_doubleSpinBox.setMaximum(loan_amount)
         self.down_amount_doubleSpinBox.setValue(down_amount_calculation(loan_amount, down_percent))
 
-
     def update_down_percent_value(self):
-        # Fetch value from the users...
+        """Update the down percent when the down amount is changing..."""
         loan_amount = self.loan_amount_doubleSpinBox.value()
         down_payment = self.down_amount_doubleSpinBox.value()
 
@@ -167,6 +222,7 @@ class MainWindow(QMainWindow):
             self.down_percent_doubleSpinBox.setValue(down_percent_calculation(down_payment, loan_amount))
 
     def calculate_output(self):
+        """Calculate the output when the calculate button is being tapped..."""
         output = output_calculation(
             self.loan_amount_doubleSpinBox.value(),
             self.down_amount_doubleSpinBox.value(),
@@ -179,8 +235,10 @@ class MainWindow(QMainWindow):
 
 app = QApplication([])
 
+# Create the primary window...
 window = MainWindow()
 
+# Show the window...
 window.show()
 
 app.exec()
--- pyqt6_calculator/controller.py
+++ pyqt6_calculator/controller.py
@@ -1,10 +1,13 @@
 def down_amount_calculation(principal_amount, down_percent):
+    """Calculate the down amount with given loan amount and down percent..."""
     return principal_amount * (down_percent / 100)
 
 def down_percent_calculation(down_amount, principal_amount):
+    """Calculate the down percent with given loan amount and down amount..."""
     return round((down_amount / principal_amount) * 100, 2)
 
 def output_calculation(principal_amount, down_amount, interest_rate, loan_duration_comboBox_currentIndex):
+    """Calculate the monthly mortgage payment..."""
     total_loan_amount = principal_amount - down_amount
     monthly_interest_rate = interest_rate / 100 / 12
     number_of_payments_arr = [120, 180, 240, 360]
@@ -13,4 +16,5 @@ def output_calculation(principal_amount, down_amount, interest_rate, loan_durati
     return monthly_mortgage_payment
 
 def convertToStringOutput(float_output):
+    """Convert and round the output to a readable string value..."""
     return f"${round(float_output, 2)}"
